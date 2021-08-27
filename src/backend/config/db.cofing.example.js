644a7fa47ce7252125dec8c07937ee323f095666
oliver.xu <929175050@qq.com>
2021-08-28 07:55:14 +0800

    /

---
+++ src/backend/config/db.cofing.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // uri: "mongodb://admin:xh19963900@8.130.48.20:27017",
+  uri: 'mongodb://[username]:[passward]@[ip]',
+  connectionOptions: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'areyoulate',
+  },
+}
