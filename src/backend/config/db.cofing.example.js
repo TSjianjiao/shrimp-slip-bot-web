2e6b1e6d3969e4a10b808cb8714ad7a362b9580f
oliver.xu <929175050@qq.com>
2021-08-28 15:40:05 +0800

    /

--- src/backend/config/db.cofing.example.js
+++ src/backend/config/db.cofing.example.js
@@ -1,7 +1,6 @@
 
 // 具体配置参考
 module.exports = {
-  // uri: "mongodb://admin:xh19963900@8.130.48.20:27017",
   uri: 'mongodb://[username]:[passward]@[ip]',
   connectionOptions: {
     useNewUrlParser: true,
