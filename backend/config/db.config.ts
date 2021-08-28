0fa48f5a4911c3adf3f085befce779c95d293338
oliver.xu <929175050@qq.com>
2021-08-29 01:14:38 +0800

    /

--- backend/config/db.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  uri: 'mongodb://admin:xh19963900@127.0.0.1:27017',
-  connectionOptions: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'areyoulate',
-  },
-}
